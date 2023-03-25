351dbfa8f464eaceee25cd042a059e8504f3634b
Saibou <saiboub80@gmail.com>
2023-03-25 14:51:10 -0400

    create and new routes complete

--- server.js
+++ server.js
@@ -26,6 +26,9 @@ app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 // MIDDLEWARE
 app.use(express.static('public'))
+// MIDDLEWARE
+app.use(express.urlencoded({extended: true}))
+
 
 // 404 Page
 app.get('*', (req, res) => {
--- views/index.jsx
+++ views/index.jsx
@@ -18,6 +18,10 @@ function Index ({breads, title})  {
     )
   })
 }
+<div className="newButton">
+  <a href="/breads/new"><button>Add a new bread</button></a>
+</div>
+
 
         </ul>
       </Default>
