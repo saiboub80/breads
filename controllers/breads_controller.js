f485452c2b6cc427c998a27d9419c3bfe849ba29
Saibou <saiboub80@gmail.com>
2023-03-26 23:48:25 -0400

    delete route complete

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -19,16 +19,17 @@ breads.get('/new', (req, res) => {
 })
 
 // SHOW
-
 breads.get('/:arrayIndex', (req, res) => {
   if (Bread[req.params.arrayIndex]) {
     res.render('Show', {
-      bread:Bread[req.params.arrayIndex]
+      bread:Bread[req.params.arrayIndex],
+      index: req.params.arrayIndex,
     })
   } else {
-    res.render('error404')
+    res.render('404')
   }
 })
+
 // CREATE
 breads.post('/', (req, res) => {
   if(!req.body.image){
@@ -42,5 +43,12 @@ breads.post('/', (req, res) => {
   Bread.push(req.body)
   res.redirect('/breads')
 })
+
+// DELETE
+breads.delete('/:indexArray', (req, res) => {
+  Bread.splice(req.params.indexArray, 1)
+  res.status(303).redirect('/breads')
+})
+
 
 module.exports = breads
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // DEPENDENCIES
 const express = require('express')
+const methodOverride = require('method-override')
 
 // CONFIGURATION
 require('dotenv').config()
@@ -7,11 +8,15 @@ const PORT = process.env.PORT
 const app = express()
 
   // MIDDLEWARE
+  // MIDDLEWARE
+  app.use(methodOverride('_method'))
   app.use(express.urlencoded({extended: true}))
   app.set('views', __dirname + '/views')
   app.set('view engine', 'jsx')
   app.engine('jsx', require('express-react-views').createEngine())
   app.use(express.static('public'))
+  
+
 
 // ROUTES
 app.get('/', (req, res) => {
@@ -23,14 +28,10 @@ const breadsController = require('./controllers/breads_controller.js')
 app.use('/breads', breadsController)
 
 
-
-
-
 // 404 Page
 app.get('*', (req, res) => {
   res.send('404')
 })
-
 
 // LISTEN
 app.listen(PORT, () => {
